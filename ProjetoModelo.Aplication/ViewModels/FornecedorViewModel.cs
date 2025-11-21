8444b0b7e14ccf178332d40af7a8af6f4bfb05fd
Alexsander-fsp <alexsanderfpsouza@gmail.com>
2025-11-21 14:46:47 -0300

    Desenvolvimento do "Fornecedor" - Em andamento

---
+++ ProjetoModelo.Aplication/ViewModels/FornecedorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Aplication.ViewModels
+{
+    class FornecedorViewModel
+    {
+        public int Id { get; set; }           // Identificador único
+        public string Nome { get; set; }      // Nome do fornecedor
+        public string CNPJ { get; set; }      // Cadastro Nacional da Pessoa Jurídica
+        public string Telefone { get; set; }  // Contato telefônico
+        public string Email { get; set; }     // Endereço de e-mail
+        public string Endereco { get; set; }  // Endereço físico
+    }
+}
